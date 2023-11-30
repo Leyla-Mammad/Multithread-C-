2f32b7ce7bb971ae96850deba81e33b83fb23786
Leyla-Mammad <leylamamedova19960@gmail.com>
2023-12-01 01:34:58 +0400

    multithread

---
+++ multithread/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        string inputText = "Hello, World!";
+        PrintText(inputText);
+    }
+
+    static void PrintText(string text)
+    {
+        foreach (char character in text)
+        {
+            Console.Write(character);
+            Thread.Sleep(1000); 
+        }
+
+        Console.WriteLine();
+    }
+}
